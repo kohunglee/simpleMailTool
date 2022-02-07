84bc1ba015168a471da56fef631625116df39a03
kohunglee <33373536+kohunglee@users.noreply.github.com>
2022-02-08 00:32:50 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'simpleMailTool.php';
+
+$port = 465;               // 端口
+$user = '46831392@qq.com'; // smtp用户名
+$pass = '--------------';// smtp密码
+$host = 'smtp.qq.com';     // 服务器地址
+
+$from   = 'kohunglee';         // 发件人昵称
+$to     = '2528852314@qq.com'; // 收件人地址
+$subjet = '标题标题';           // 信件标题
+$body   = '<h1>hi</h1>the mail sent successful';           // 信件正文
+
+$test = new simpleMailTool($host,$port,$user,$pass,true);
+
+
+// 验证用户名密码是否能连接到服务器
+// echo ($test->verifyUser($from,$to)) ? '连接成功' : '连接失败，用户名或密码错误' ;
+
+// 发邮件测试 , 参数(发件人昵称、收件人地址、信件标题、信件正文)
+// $test->sendMail($from,$to,$subjet,$body);
+
+?>
