1154bd928f9a302eab9d583a919f287672f7587f
kohunglee <33373536+kohunglee@users.noreply.github.com>
2022-02-09 00:05:22 +0800

    Update test.php

--- test.php
+++ test.php
@@ -14,7 +14,7 @@ $body   = '<h1>hi</h1>the mail sent successful';           // 信件正文
 $test = new simpleMailTool($host,$port,$user,$pass,true);  // 关闭调试只需将最后一个参数删去
 
 // 验证用户名密码是否能连接到服务器
-// echo ($test->verifyUser()) ? '连接成功' : '连接失败，用户名或密码错误' ;
+// echo ($test->verifyUser()) ? '<br>连接成功' : '<br>连接失败，用户名或密码错误' ;
 
 // 发邮件测试 , 参数(发件人昵称、收件人地址、信件标题、信件正文)
 // $test->sendMail($from,$to,$subjet,$body);
